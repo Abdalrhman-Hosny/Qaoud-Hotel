09f5da8ba4cfcf6a386ff649a8d5547b0fea3209
Abdalrhman Hosny <hsnyb2762@gmail.com>
2025-08-04 21:15:22 +0200

    finish main content of dashboard file

--- back-end/admin/settings.php
+++ back-end/admin/settings.php
@@ -7,7 +7,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin Panel - Dashboard</title>
+    <title>Admin Panel - Setting</title>
     <!-- Bootstrap CSS link -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
     <!-- Bootstrap icons link -->
@@ -46,7 +46,7 @@
                             <a class="nav-link text-white " href="#">Users</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link text-white " href="settings.php">Settings</a>
+                            <a class="nav-link text-white " href="#">Settings</a>
                         </li>
                     </ul>
                 </div>
